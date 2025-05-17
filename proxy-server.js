73b5777abdb2c3391bb5337ff094929b4a569e8b
HUYNH VAN CHI KHAN <huynhkhan91@gmail.com>
2025-05-18 02:57:08 +0700

    'v'

--- proxy-server.js
+++ proxy-server.js
@@ -44,6 +44,7 @@ app.get("/get-ip", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Proxy server listening on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
